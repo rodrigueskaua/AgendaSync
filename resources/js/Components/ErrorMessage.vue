24ce0a6ac2c5cbad32435a17cf2dba77005900b9
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-08-22 20:35:25 -0300

    feat(components): adiciona componente ErrorMessage

---
+++ resources/js/Components/ErrorMessage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="message" class="error-message">
+    {{ message }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  message: {
+    type: String,
+    default: ''
+  }
+});
+</script>
+
+<style scoped>
+.error-message {
+  color: #f44336;
+  font-size: 0.875em;
+  margin-top: 0.25rem;
+}
+</style>
