c45c22438b1171d5885a5e6ae8fd5028ff693d43
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-08-23 13:14:12 -0300

    feat(seeder): adiciona seeder para preencher a tabela contact_books

---
+++ database/seeders/ContactBookSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+class ContactBookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+      $faker = Faker::create();
+
+      foreach (range(1, 50) as $index) {
+          DB::table('contact_books')->insert([
+              'id' => $faker->uuid,
+              'name' => $faker->name,
+              'email' => $faker->unique()->safeEmail,
+              'phone' => $faker->phoneNumber,
+              'address' => $faker->address,
+              'created_at' => now(),
+              'updated_at' => now(),
+          ]);
+      }
+    }
+}
