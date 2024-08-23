d95ec65297d927637f727811fbf3a5a97053bc16
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-08-22 23:44:24 -0300

    feat(models, migrations): cria model ContactBook e migração para tabela contactbooks

---
+++ app/Models/ContactBook.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class ContactBook extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
+}
---
+++ database/migrations/2024_08_23_020302_create_contact_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_books', function (Blueprint $table) {
+          $table->string('id')->primary();
+          $table->string('name');
+          $table->string('email')->unique();
+          $table->string('phone')->nullable();
+          $table->text('address')->nullable();
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_books');
+    }
+};
