27c8c9fd178438bafbb95f6076d6833f996205b0
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-08-23 14:12:20 -0300

    feat(components): cria ContactList com exibição de contatos e suporte a Searchbar

---
+++ resources/js/Components/ContactList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+    <div class="col" v-for="contact in contacts" :key="contact.id">
+      <ContactCard
+        :contactName="contact.name"
+        :contactDetails="contact.email ? contact.email : contact.phone"
+        :contactId="contact.id"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ContactCard from '@/Components/ContactCard.vue';
+const props = defineProps({
+  contacts: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+</script>
