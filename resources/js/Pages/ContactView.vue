f6ea4ddba28a0ff4566b9101ffc13118eac9c14a
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-08-24 00:48:15 -0300

    refactor(ContactView): refatora ContactView para utilizar os componentes ButtonContainer e ButtonWrapper

--- resources/js/Pages/ContactView.vue
+++ resources/js/Pages/ContactView.vue
@@ -11,34 +11,7 @@
       </div>
     </div>
 
-    <div class="buttons-container">
-      <div class="action-buttons">
-        <div class="btn-wrapper">
-          <button class="btn btn-primary" @click="callUser">
-            <i class='bx bx-phone'></i>
-          </button>
-          <p class="btn-text">Ligar</p>
-        </div>
-        <div class="btn-wrapper">
-          <button class="btn btn-primary" @click="emailUser">
-            <i class='bx bx-envelope'></i>
-          </button>
-          <p class="btn-text">Enviar Email</p>
-        </div>
-        <div class="btn-wrapper">
-          <button class="btn btn-outline-primary" @click="editUser">
-            <i class='bx bx-edit'></i>
-          </button>
-          <p class="btn-text">Editar</p>
-        </div>
-        <div class="btn-wrapper">
-          <button class="btn btn-outline-danger" @click="deleteUser">
-            <i class='bx bx-trash'></i>
-          </button>
-          <p class="btn-text">Apagar</p>
-        </div>
-      </div>
-    </div>
+    <ButtonContainer :buttons="buttons" />
     
     <div v-if="showAlert" class="alert alert-info alert-dismissible fade show" role="alert">
       <strong>Atenção!</strong> {{ alertMessage }}
@@ -71,6 +44,35 @@
 <script setup>
 import { Inertia } from '@inertiajs/inertia';
 import Navbar from '@/Components/Navbar.vue';
+import ButtonContainer from '@/Components/ButtonContainer.vue';
+
+const buttons = [
+  {
+    icon: 'bx bx-phone',
+    text: 'Ligar',
+    buttonClass: 'btn btn-primary',
+    onClick: () => { callUser(); }
+  },
+  {
+    icon: 'bx bx-envelope',
+    text: 'Email',
+    buttonClass: 'btn btn-primary',
+    onClick: () => { emailUser(); }
+  },
+  {
+    icon: 'bx bx-edit',
+    text: 'Editar',
+    buttonClass: 'btn btn-primary',
+    onClick: () => { editUser() }
+  },
+  {
+    icon: 'bx bx-trash',
+    text: 'Apagar',
+    buttonClass: 'btn btn-secondary',
+    onClick: () => { deleteUser() }
+  }
+];
+
 import { ref, defineProps, onMounted } from 'vue';
 import Swal from 'sweetalert2';
 
@@ -187,125 +189,6 @@ function deleteUser() {
   font-size: 24px;
   font-weight: bold;
   margin: 0;
-}
-
-.buttons-container {
-  width: 100%;
-  margin-bottom: 20px;
-  padding: 20px;
-  background: #ffffff;
-  border-radius: 12px;
-  border: 1.5px solid rgba(67, 67, 67, 0.15);
-  box-shadow: -1px -1px 30px rgba(73, 73, 73, 0.08);
-}
-
-.action-buttons {
-  display: flex;
-  justify-content: center;
-  flex-direction: row!important;
-  gap: 45px!important; 
-}
-
-.btn-wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.btn {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 80px;
-  height: 80px;
-  padding: 0;
-  border: none;
-  border-radius: 50%;
-  font-size: 24px;
-  transition: background-color 0.3s, box-shadow 0.3s;
-}
-
-.action-buttons {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.btn {
-  font-size: 32px;
-  width: 70px;
-  height: 70px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  padding: 0;
-  border: none;
-  transition: background-color 0.3s, box-shadow 0.3s;
-  text-align: center;
-}
-
-.btn-text {
-  line-height: 0;
-  font-size: 15px;
-  margin-top: 20px;
-}
-
-@media (max-width: 768px) {
-  .btn {
-    font-size: 24px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .btn-text {
-    font-size: 13px;
-  }
-}
-
-.btn-primary {
-  background-color: var(--primary);
-  color: #fff;
-}
-
-.btn-primary:hover {
-  background-color: var(--primary-dark);
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-.btn-secondary {
-  background-color: #6c757d;
-  color: #fff;
-}
-
-.btn-secondary:hover {
-  background-color: #5a6268;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-.btn-outline-primary {
-  background-color: transparent;
-  color: var(--primary-dark);
-  border: 2px solid var(--primary-dark);
-}
-
-.btn-outline-primary:hover {
-  background-color: var(--primary-dark);
-  color: #fff;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-.btn-outline-danger {
-  background-color: transparent;
-  color: var(--primary-dark);
-  border: 2px solid var(--primary-dark);
-}
-
-.btn-outline-danger:hover {
-  background-color: #db140d;
-  border: 2px solid #db140d;
-  color: #fff;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 .info-container {
