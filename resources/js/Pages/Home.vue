275a8e42fbd2baf5819468af13b9bd5add52a555
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-08-23 23:02:56 -0300

    feat(home): adiciona Notification à Home.vue e função para exibir os alertas temporários

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="container">
     <Navbar />
-    
+
     <SearchBar v-model="search" />
 
-    <ContactList :contacts="filteredContacts" />         
+    <Notification
+      v-if="successMessage"
+      :message="notificationMessage"
+      :icon="notificationIcon" />
+
+    <ContactList :contacts="filteredContacts" />
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import Navbar from '@/Components/Navbar.vue';
 import SearchBar from '@/Components/SearchBar.vue';
 import ContactList from '@/Components/ContactList.vue';
+import Notification from '@/Components/Notification.vue';
 
 const props = defineProps({
-  contacts: Array
+  contacts: Array,
+  successMessage: String
 });
 
 const search = ref('');
@@ -29,7 +36,27 @@ const filteredContacts = computed(() => {
     contact.email.toLowerCase().includes(search.value.toLowerCase())
   );
 });
+
+const notificationMessage = ref('');
+const notificationIcon = ref('bx bx-info-circle');
+
+function showNotification(message, icon = 'bx bx-info-circle') {
+  notificationMessage.value = message;
+  notificationIcon.value = icon;
+  setTimeout(() => {
+    notificationMessage.value = '';
+  }, 5000);
+}
+
+console.log(props.successMessage)
+
+
+onMounted(() => {
+  if (props.successMessage) {
+    showNotification(props.successMessage, 'bx bx-check-circle');
+  }
+});
+
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
