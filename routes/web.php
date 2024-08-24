f94a1edc73badce8e1e6f5e3fed01d15415779bf
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-08-23 23:20:14 -0300

    chore(middleware): adiciona middleware de autenticação e guest para controle de acesso nas rotas

--- routes/web.php
+++ routes/web.php
@@ -7,35 +7,31 @@ use Inertia\Inertia;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ContactBookController;
 
-Route::get('/', [ContactBookController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+  Route::get('/', [ContactBookController::class, 'index'])->name('home');
 
-Route::get('/contact/create ', function () {
-  return Inertia::render('ContactCreate');
-})->name('contact.register');
+  Route::get('/contact/create', function () {
+      return Inertia::render('ContactCreate');
+  })->name('contact.register');
 
-Route::post('/contact/create',[ContactBookController::class, 'create'])
-->name('contact.create');
+  Route::post('/contact/create', [ContactBookController::class, 'create'])->name('contact.create');
+  Route::put('/contact/update/{id}', [ContactBookController::class, 'update'])->name('contact.update');
+  Route::get('/contact/update/{id}', [ContactBookController::class, 'edit'])->name('contact.edit');
+  Route::delete('/contact/destroy/{id}', [ContactBookController::class, 'destroy'])->name('contact.destroy');
+  Route::get('/contact/{id}', [ContactBookController::class, 'show'])->name('contact.show');
 
-Route::put('/contact/update/{id}', [ContactBookController::class, 'update'])->name('contact.update');
+  Route::get('/logout', [UserController::class, 'logout'])->name('user.logout');
+});
 
-Route::get('/contact/update/{id}', [ContactBookController::class, 'edit'])->name('contact.edit');
+Route::middleware(['guest'])->group(function () {
+  Route::get('/login', function () {
+      return Inertia::render('Login');
+  })->name('login');
 
-Route::delete('/contact/destroy/{id}', [ContactBookController::class, 'destroy'])->name('contact.destroy');
+  Route::get('/register', function () {
+      return Inertia::render('Register');
+  })->name('register');
 
-Route::get('/contact/{id}', [ContactBookController::class, 'show'])->name('contact.show');
-
-Route::get('/login', function () {
-  return Inertia::render('Login');
-})->name('login');
-
-Route::get('/register', function () {
-  return Inertia::render('Register');
-})->name('register');
-
-Route::post('/user/create',[UserController::class, 'create'])
-->name('user.create');
-
-Route::post('login',[UserController::class, 'login'])
-->name('user.login');
-
-Route::get('/logout', [UserController::class, 'logout'])->name('user.logout');
+  Route::post('/user/create', [UserController::class, 'create'])->name('user.create');
+  Route::post('login', [UserController::class, 'login'])->name('user.login');
+});
